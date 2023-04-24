945ae11cac31ebb9616cf2116a4e312c9482027f
Nathan McCloskey <49048346+Rudamentary99@users.noreply.github.com>
2023-04-24 15:38:16 -0500

    added zsh plugins

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,7 +1,39 @@
-{ ... }:
+{ pkgs
+, ...
+}:
 
 {
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "thefuck"
+        "z"
+        "vi-mode"
+        "command-not-found"
+        "alias-finder"
+        "aliases"
+        "common-aliases"
+
+      ];
+    };
+    plugins =
+      [
+        {
+          # will source zsh-autosuggestions.plugin.zsh
+          name = "zsh-autosuggestions";
+          src = pkgs.fetchFromGitHub
+            {
+              owner = "zsh-users";
+              repo = "zsh-autosuggestions";
+              rev = "v0.4.0";
+              sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+            };
+        }
+      ];
+  };
   # This must be envExtra (rather than initExtra), because doom-emacs requires it
   # https://github.com/doomemacs/doomemacs/issues/687#issuecomment-409889275
   #
